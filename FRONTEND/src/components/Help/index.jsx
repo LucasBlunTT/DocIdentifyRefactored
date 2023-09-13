950049cb8c62e2877f7376dae6b1b0720cbe9ecf
Lucas da Silva <sklucassilva@gmail.com>
2023-09-13 19:43:40 -0300

    correção font de ajuda

--- FRONTEND/src/components/Help/index.jsx
+++ FRONTEND/src/components/Help/index.jsx
@@ -7,7 +7,7 @@ function Help() {
     <ul className="absolute left-[-357px] flex items-center justify-center">
       <li className="flex items-center justify-center">
         <Image className="mr-2" src={escIcon} alt="esc icon" />
-        <p>PARA LIMPAR TUDO</p>
+        <strong>PARA LIMPAR TUDO</strong>
       </li>
     </ul>
   );
