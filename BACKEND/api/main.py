1eb7b17fd034c1638e13ba665a917b48e9a51f51
Luan Pacheco <luan_pacheco@hotmail.com>
2023-12-04 17:26:51 -0300

    ajuste 2

--- BACKEND/api/main.py
+++ BACKEND/api/main.py
@@ -1,19 +1,10 @@
-# Importar os módulos necessários
 from utilitarios import extrair_dados_documento
 from flask import jsonify
 
 def main(caminho):
-    # Definir o caminho para o executável do Tesseract OCR
-    
-
-    # Caminho da imagem
-    #caminho = ".//BACKEND//data//images//cnh.jpg"  
-    #caminho = ".//BACKEND//data//images//cnh2.jpeg"
 
     # Chamar a função para processar a imagem do documento
     dados_documento = extrair_dados_documento(caminho)
-
-    #mostrar dados do documento
     
     return jsonify(dados_documento)
 
